219012061a075894d10368f747c51d7bdd824a76
Alan-52 <alanbetancurbedoya13@gmail.com>
2025-05-06 18:40:10 -0500

    se crean las funciones para el login y para mostrar el menu principal dentro del archivo main.py

--- main.py
+++ main.py
@@ -29,3 +29,33 @@ def registrar_usuario():
     conn.commit()
     print("\n¡Registro exitoso! Puedes iniciar sesión desde el menú principal.")
     conn.close()
+
+# Función para realizar el login
+def login():
+    username = input("\nIngrese su nombre de usuario: ")
+    password = input("Ingrese su contraseña: ")
+
+    # Encriptar la contraseña ingresada
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+    # Conectar a la base de datos
+    conn = conectar_db()
+    cursor = conn.cursor()
+
+    # Verificar si el usuario existe en la base de datos
+    cursor.execute("SELECT * FROM usuarios WHERE username = %s AND password = %s", (username, password_hash))
+    user = cursor.fetchone()
+
+    if user:
+        print(f"\n   Bienvenido, {user[1]}!")
+        return user
+    else:
+        print("\nUsuario o contraseña incorrectos.")
+        return None
+
+# Función para mostrar el menú principal
+def mostrar_menu():
+    print("\n--- Menú ---")
+    print("1. Iniciar sesión")
+    print("2. Registrarse")
+    print("3. Salir del programa")
